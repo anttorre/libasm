b42a0176990066bda297724d1d69640770190480
anttorre <atormora@gmail.com>
2026-03-11 02:22:11 +0100

    add strcpy fixed

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -2,14 +2,14 @@ global ft_strcpy
 section .text
 
 ft_strcpy:
-    mov rcx, 0              ; Usamos rcx como índice (i = 0).
+    mov rcx, 0
 .loop:
-    mov al, [rsi + rcx]     ; Movemos el carácter actual de src al registro de 8 bits 'al'.
-    mov [rdi + rcx], al     ; Copiamos ese carácter 'al' a la posición correspondiente en dest.
-    cmp al, 0               ; ¿Acabamos de copiar el carácter nulo '\0'?
-    je .done                ; Si es 0, terminamos.
-    inc rcx                 ; Incrementamos el índice (i++).
-    jmp .loop               ; Repetimos.
+    mov al, [rsi + rcx]
+    mov [rdi + rcx], al
+    cmp al, 0
+    je .done
+    inc rcx
+    jmp .loop
 .done:
-    mov rax, rdi            ; La función strcpy debe retornar el puntero al destino (rdi).
+    mov rax, rdi
     ret
